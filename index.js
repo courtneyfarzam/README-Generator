7e7672fac545ab25c967b234af0dd2857d4c970b
cmash93 <cmash93@gmail.com>
2022-10-01 22:30:34 -0500

    moved files around

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+.prompt([
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+])
+.then((data) => {
+
+})
+.catch((error) => {
+    if (error.isTtyError){
+
+    } else {
+
+    }
+})
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
